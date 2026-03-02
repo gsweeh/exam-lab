c4aa27ff0970645b9d13773209f84381086a5fdc
=Prabha <you@example.com>
2026-03-02 11:42:27 +0530

    Added by prathiksha

--- lab.c
+++ lab.c
@@ -20,6 +20,14 @@ void printArray(int arr[],int n){
     printf("%d", arr[i]);
   }
  printf("\n");
+
+}
+
+void printArray(int arr[],int n){
+   for(int i=0;i<n;i++){
+    printf("%d", arr[i]);
+  }
+ printf("I value\n");
 }
 
 int main(){
