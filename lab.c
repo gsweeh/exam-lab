0e1847b26601c50f5a1b9416a4e418c201297744
=Prabhava <ananyahegde533@gmail.com>
2026-03-02 10:53:13 +0530

    commit by pankaj

---
+++ lab.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    // Traverse through all array elements
+    for (int i = 0; i < n - 1; i++) {
+        // Last i elements are already in place
+        for (int j = 0; j < n - i - 1; j++) {
+            // Traverse the array from 0 to n-i-1
+            // Swap if the element found is greater than the next element
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
